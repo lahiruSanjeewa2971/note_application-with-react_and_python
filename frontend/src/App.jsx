7dde6a4449c14fbc6d5bdc7fd4856a96066466a6
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-10-24 00:01:22 +0530

    Create ProtectedRoute component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,20 @@
-import react from 'react'
+import react from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndMakeLogout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
 
 function App() {
   return <></>;
